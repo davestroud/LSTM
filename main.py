9f17bf130a2b2eaae2368199d327e414deda2371
davestroud <david@davidstroud.me>
2019-11-18 22:10:42 +0000

    wrote functions to prepare for lstm

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
- # this will be the main file
+ #!/usr/bin/python3
+
+
+import random as np
+import math
+
+def sigmoid(x):
+    return 1. / (1 + np.exp(-x))
+
+def sigmoid_derivative(values):
+    return values*(1-values)
+
+def tanh_derivatives(values):
+    return 1. - values ** 2
+
+# create uniform random array w/ values in [a,b] and shape args
+
+def rand_arr(a, b, *args):
+    np.random.seed(0)
+    return np.random.rand(*args) * (b - a) + a
+
+
